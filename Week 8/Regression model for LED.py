cfe58addbf42778a746113940c4c1369fb0336fb
Ericdiii <70551470+Ericdiii@users.noreply.github.com>
2020-09-26 14:29:34 +1000

    Create Regression model for LED.py

---
+++ Week 8/Regression model for LED.py
@@ -0,0 +1,34 @@
+# Insert needed package
+import pandas as pd
+import numpy as np
+
+# Insert dataset
+import os
+print (os.path.abspath('.'))
+data_file = pd.read_csv('Dataset_LED.csv')
+df = pd.read_csv("Dataset_LED.csv")
+
+# Take a look at the dataset
+df.head()
+# Summarize the data
+df.describe()
+cdf = df[['Current','Status']]
+cdf.head(9)
+viz = cdf[['Current','Status']]
+viz.hist()
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+# Simple linear regression model
+
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['Current']])
+train_y = np.asanyarray(train[['Status']])
+regr.fit (train_x, train_y)
+
+# Save the model
+import joblib
+joblib.dump(regr, 'Model_LED.pkl')
